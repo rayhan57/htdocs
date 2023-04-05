8e22a716108de9d4e5fee718c23de9b38328c240
rayhan57 <rayhanb18@gmail.com>
2023-04-05 22:18:53 +0700

    update

---
+++ belajarphp/phpmvc/app/core/App.php
@@ -0,0 +1,16 @@
+<?php
+
+class App {
+    public function __construct() {
+        var_dump(App::parseURL());
+    }
+
+    public function parseURL() {
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
+    }
+}
