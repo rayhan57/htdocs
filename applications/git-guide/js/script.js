e905f94733f364a2fb46b565161b0be276df9055
rayhan57 <rayhanb18@gmail.com>
2023-05-07 23:30:13 +0700

    update

--- applications/git-guide/js/script.js
+++ applications/git-guide/js/script.js
@@ -4,13 +4,6 @@ fetch("data/tutorial.json")
   .then((response) => response.json())
   .then((data) => {
     data.forEach((data) => {
-      let langkah = data.langkah;
-      let langkahHTML = "";
-
-      langkah.forEach((langkah) => {
-        langkahHTML += `<li class="list-group-item">${langkah}</li>`;
-      });
-
       content.innerHTML += `<div class="col-md-4">
           <div class="card mb-3">
             <img src="https://source.unsplash.com/200x100?${data.slug}" class="card-img-top" alt="${data.judul}" />
@@ -23,15 +16,24 @@ fetch("data/tutorial.json")
           </div>
         </div>`;
     });
+    window.addEventListener("hashchange", handleRoute);
+    handleRoute(data);
   })
   .catch((error) => console.error(error));
 
-function handleRoute() {
+function handleRoute(data) {
   const route = window.location.hash.substr(2);
+  let langkah = data[0].langkah;
+  let langkahHTML = "";
+
+  langkah.forEach((step) => {
+    langkahHTML += `<li class="list-group-item">${step}</li>`;
+  });
 
   switch (route) {
     case "register":
-      content.innerHTML = `<h1>${Data.judul}</h1><ol class="list-group list-group-numbered">${langkahHTML}</ol>`;
+      console.log(data);
+      content.innerHTML = `<h1>${data[0].judul}</h1><ol class="list-group list-group-numbered">${langkahHTML}</ol>`;
       break;
     case "contact":
       content.innerHTML = "<h1>Contact</h1><p>Ini adalah halaman contact</p>";
@@ -39,5 +41,3 @@ function handleRoute() {
     default:
   }
 }
-window.addEventListener("hashchange", handleRoute);
-handleRoute();
