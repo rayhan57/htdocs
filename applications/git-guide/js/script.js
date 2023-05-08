565a774b54c833e27fb1b696b33b060edcbcafa8
rayhan57 <rayhanb18@gmail.com>
2023-05-08 23:31:27 +0700

    update

--- applications/git-guide/js/script.js
+++ applications/git-guide/js/script.js
@@ -4,40 +4,26 @@ fetch("data/tutorial.json")
   .then((response) => response.json())
   .then((data) => {
     data.forEach((data) => {
+      let langkahHTML = "";
+      let langkah = data.langkah;
+
+      langkah.forEach((step) => {
+        langkahHTML += `<li class="list-group-item list-group-item-light">${step}</li>`;
+      });
+
       content.innerHTML += `<div class="col-md-4">
-          <div class="card mb-3">
-            <img src="https://source.unsplash.com/200x100?${data.slug}" class="card-img-top" alt="${data.judul}" />
-            <div class="card-body">
-              <p class="card-text">
-                ${data.deskripsi}
-              </p>
-              <a href="#/${data.slug}" class="card-link btn btn-primary">Baca</a>
-            </div>
-          </div>
-        </div>`;
+                            <div class="card mb-3 border border-0 shadow bg-light">
+                              <img src="https://source.unsplash.com/1000x500?${data.slug}" class="card-img-top" alt="${data.slug}">
+                              <div class="card-body border-bottom">
+                                <h5 class="card-title">${data.judul}</h5>
+                                <p class="card-text">${data.deskripsi}</p>
+                              </div>
+                              <ul class="list-group list-group-flush">
+                              <p class="fs-5 mx-3 pt-2 fw-semibold">Langkah-langkah:</p>
+                                ${langkahHTML}
+                              </ul>
+                            </div>
+                            </div>`;
     });
-    window.addEventListener("hashchange", handleRoute);
-    handleRoute(data);
   })
   .catch((error) => console.error(error));
-
-function handleRoute(data) {
-  const route = window.location.hash.substr(2);
-  let langkah = data[0].langkah;
-  let langkahHTML = "";
-
-  langkah.forEach((step) => {
-    langkahHTML += `<li class="list-group-item">${step}</li>`;
-  });
-
-  switch (route) {
-    case "register":
-      console.log(data);
-      content.innerHTML = `<h1>${data[0].judul}</h1><ol class="list-group list-group-numbered">${langkahHTML}</ol>`;
-      break;
-    case "contact":
-      content.innerHTML = "<h1>Contact</h1><p>Ini adalah halaman contact</p>";
-      break;
-    default:
-  }
-}
