efabbcdfcde05345947ad80104f26084f73e0426
rayhan57 <rayhanb18@gmail.com>
2023-06-01 10:18:12 +0700

    update

--- applications/git-guide/js/script.js
+++ applications/git-guide/js/script.js
@@ -12,7 +12,7 @@ fetch("data/tutorial.json")
       });
 
       content.innerHTML += `<div class="col-md-4">
-                            <div class="card mb-3 border border-0 shadow bg-light">
+                            <div class="card mb-3 border border-0 shadow text-bg-light">
                               <img src="${data.gambar}" class="card-img-top" alt="${data.gambar}">
                               <div class="card-body border-bottom">
                                 <h5 class="card-title">${data.judul}</h5>
